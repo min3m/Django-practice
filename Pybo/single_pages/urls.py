76aef84e32e8c75225c0cefecde2041616d4b214
youngmin <youngmin@youngminui-MacBookPro.local>
2024-01-09 16:21:38 +0900

    single_page & about_me -> FBV

---
+++ Pybo/single_pages/urls.py
@@ -0,0 +1,9 @@
+#single pages urls
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('about_me/', views.about_me),
+	path('', views.landing)
+]
+
--- Pybo/single_pages/views.py
+++ Pybo/single_pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#대문 페이지 랜더링 해주는 부분
+def landing(request):
+    return render(
+		request,
+		'single_pages/landing.html'
+	)
+
+#about me 페이지 랜더링 해주는 부분
+def about_me(request):
+    return render(
+		request,
+		'single_pages/about_me.html'
+	)
